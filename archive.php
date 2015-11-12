3bd2c41818d82cba552b714b406fce799f6cc88b
Sami Keijonen <sami.keijonen@foxnet.fi>
2015-11-12 17:57:31 +0200

    Add achive-title class. Reconsider do we even need this file.

--- archive.php
+++ archive.php
@@ -11,10 +11,10 @@ get_header(); ?>
 
 	<?php if ( have_posts() ) : ?>
 
-		<header class="page-header">
+		<header class="page-header archive-header">
 			<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="taxonomy-description">', '</div>' );
+				the_archive_title( '<h1 class="page-title archive-title">', '</h1>' );
+				the_archive_description( '<div class="taxonomy-description archive-description">', '</div>' );
 			?>
 		</header><!-- .page-header -->
 
