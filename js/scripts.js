909a0f191b548c57232295bdc8b789d83feedeed
Sami Keijonen <sami.keijonen@foxnet.fi>
2015-11-17 14:53:05 +0200

    Remove variables we are not using.

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,6 @@
  */
 ( function() {
 	
-	// Get window width.
-	var w = window,
-		e = document.documentElement,
-		g = document.getElementsByTagName( 'body' )[0],
-		x = w.innerWidth || e.clientWidth || g.clientWidth;
-	
 	// Initialize perfect scrollbar.
 	var content   = document.getElementById( 'content' );
 	var header    = document.getElementById( 'masthead' );
