b243c1d481bbef5b5321e1ac3e997abcc22ad7d8
Sami Keijonen <sami.keijonen@foxnet.fi>
2015-12-17 13:42:28 +0200

    Put Jetpack related stuff in the same file.

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -12,12 +12,29 @@
  * See: https://jetpack.me/support/infinite-scroll/
  */
 function sonsa_jetpack_setup() {
+	
+	// Add theme support for infinite scroll.
 	add_theme_support( 'infinite-scroll', array(
 		'type'      => 'click',
 		'container' => 'main',
 		'render'    => 'sonsa_infinite_scroll_render',
 		'footer'    => 'content',
 	) );
+	
+	// Add theme support for portfolio.
+	add_theme_support( 'jetpack-portfolio' );
+	
+	// Add theme support for site logo.
+	add_theme_support( 'site-logo', array(
+		'size' => 'sonsa-site-logo',
+	) );
+	
+	// Add theme support for responsive videos.
+	add_theme_support( 'jetpack-responsive-videos' );
+	
+	// Add theme support for Jetpack food menus.
+	add_theme_support( 'nova_menu_item' );
+	
 } // end function sonsa_jetpack_setup
 add_action( 'after_setup_theme', 'sonsa_jetpack_setup' );
 
