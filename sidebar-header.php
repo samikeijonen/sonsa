44d9a81d597aab327b95ce27f8822f6061b03d49
Sami Keijonen <sami.keijonen@foxnet.fi>
2016-01-13 12:21:44 +0200

    Remove heading from the header sidebar. It's kind of confusin.

--- sidebar-header.php
+++ sidebar-header.php
@@ -9,7 +9,6 @@
 <?php if ( is_active_sidebar( 'header' ) ) : // If the sidebar has widgets. ?>
 
 	<aside id="sidebar-header" class="sidebar-header sidebar" role="complementary" <?php hybrid_attr( 'sidebar', 'header' ); ?>>
-		<h2 class="screen-reader-text" id="sidebar-header-header"><?php echo esc_attr_x( 'Header Sidebar', 'Sidebar aria label', 'sonsa' ); ?></h2>
 		
 		<div class="wrap">
 		
