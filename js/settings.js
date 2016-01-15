1b75a3579f7fa6e894c529615052050bc49bb8fa
Sami Keijonen <sami.keijonen@foxnet.fi>
2016-01-15 14:27:33 +0200

    Create better keyboard navigation when you can now stay inside sidebar when using tab or shift + tab.

--- js/settings.js
+++ js/settings.js
@@ -8,10 +8,10 @@
 	var body, page, mainNav, menuButton, menuToggle;
 	
 	// Set up vars.
-	page               = $( '#page' );
-	mainNav            = page.find( '#secondary' );
-	menuButton         = page.find( '#sidebar-nav-toggle' );
-	menuToggle         = page.find( '.sidebar-nav-toggle' );
+	page       = $( '#page' );
+	mainNav    = page.find( '#sonsa-secondary' );
+	menuButton = page.find( '#sidebar-nav-toggle' );
+	menuToggle = page.find( '.sidebar-nav-toggle' );
 	
 	// Preload page and fade the content.
 	$( window ).load( function() { // makes sure the whole site is loaded
@@ -100,5 +100,54 @@
 		}
 		
 	});
+	
+	// Add the logic for tab and shift+tab inside Sidebar.
+	function onResizeFocus() {
+		
+		var focusable, firstFocusable, lastFocusable;
+	
+		// Get all, first and last focusable elements from the Sidebar.
+		focusable      = mainNav.find( 'select, input, textarea, button, a' ).filter( ':visible' );
+		firstFocusable = focusable.first();
+		lastFocusable  = focusable.last();
+
+		// Redirect last tab to first input or to menu button.
+		lastFocusable.on( 'keydown', function ( e ) {
+			if ( ( e.keyCode === 9 && !e.shiftKey ) ) {
+				e.preventDefault();
+				
+				if ( window.innerWidth < 960 ) {
+					firstFocusable.focus(); // Set focus on first element - that's actually close menu button.
+				} else {
+					menuButton.focus(); // Set focus on menu button.
+				}
+				
+			}
+		});
+
+		// Redirect first shift+tab to last input or to menu button.
+		firstFocusable.on( 'keydown', function ( e ) {
+			if ( ( e.keyCode === 9 && e.shiftKey ) ) {
+				e.preventDefault();
+		
+				if ( window.innerWidth < 960 ) {
+					lastFocusable.focus(); // Set focus on last element.
+				} else {
+					menuButton.focus(); // Set focus on menu button.
+				}
+				
+			}
+		});
+
+	}
+	
+	// Call for onResizeFocus function.
+	$( document ).ready( function() {
+		$( window )
+			.on( 'load', onResizeFocus )
+			.on( 'resize', function() {
+				onResizeFocus();
+			} );
+	} );
 	
 } )( jQuery );
